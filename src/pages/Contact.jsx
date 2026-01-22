a9693f5c2e712544c6b5322ed0308b882341c426
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-23 00:13:51 +0700

    feat: add navbar component to Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,9 @@
+import { Navbar } from "../component/Navbar"
+
 export const Contact = ()=>{
     return(
         <div>
-            <h1>Contact</h1>
+            <Navbar contact={"text-blue-600"} />
         </div>
     )
 }
